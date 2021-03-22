6778fa504938cd3c37c12c199c70f717b3c5ca78
cesar <caacesar.ariza@gmail.com>
2021-03-21 19:14:12 -0500

    se camia para que funcione en spring

---
+++ src/main/java/co/edu/ucentral/Alistar.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Alistar {
+	public static void main(String[] args) {
+		SpringApplication.run(Alistar.class, args);
+	}
+}
