6ec313704689c237512f634941e2c4ab239bef4a
cesar <caacesar.ariza@gmail.com>
2021-05-04 19:59:03 -0500

    se actualiza el servicio
    se realiza la actualizacion

--- src/main/java/co/edu/ucentral/controllerRest/EventoCtrl.java
+++ src/main/java/co/edu/ucentral/controllerRest/EventoCtrl.java
@@ -10,27 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.edu.ucentral.dto.EventoDTO;
+import co.edu.ucentral.dto.ResponseDto;
 import co.edu.ucentral.entidades.Evento;
 import co.edu.ucentral.entidades.Usuario;
 import co.edu.ucentral.repository.IEventoRepository;
 import co.edu.ucentral.repository.IUsuariosRepository;
-import co.edu.ucentral.services.ServiceEvento;
 
 @RestController
 @RequestMapping(path = "/Evento")
 public class EventoCtrl {
 
-	@Autowired
-	private ServiceEvento serviceEvento;
+
 	@Autowired
 	private IEventoRepository eventoRepository;
 	@Autowired
 	private IUsuariosRepository usuarioRepository;
+	
 	@GetMapping(path = "Get-evento-Usuario",params ="usuarioId" )
 	public ResponseEntity<?> getEventoUsuario(@RequestParam(required = true,value = "usuarioId") Integer usuarioId ){
 		Usuario usuario = usuarioRepository.findByNumeroIdentificacion(usuarioId);
 		List<Evento> listEvento= eventoRepository.findByUsuario(usuario);
+		ResponseDto responseDto = new ResponseDto();
+		if(listEvento.isEmpty()) {
+			responseDto.setMensaje("No se encuetra ningun servicio");
+			return new ResponseEntity<>(responseDto,HttpStatus.NO_CONTENT);
+		}
 		return new ResponseEntity<>(listEvento,HttpStatus.OK);
 	}
 	
