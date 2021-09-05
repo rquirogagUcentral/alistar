d71992d48a1ce2da99049d4ddfdd161be1f03e9a
cesar <caacesar.ariza@gmail.com>
2021-09-04 23:19:44 -0500

    Squashed commit of the following:
    commit 43f8bab98810d9a5eaa5f9e3c64251f174edf3ac
    Author: cesar <caacesar.ariza@gmail.com>
    Date:   Sat Sep 04 23:16:47 2021 -0500
    
    	cambios de Featuer

---
+++ src/main/java/co/edu/ucentral/dto/RespuestaGenerica.java
@@ -0,0 +1,29 @@
+package co.edu.ucentral.dto;
+
+public class RespuestaGenerica {
+
+	private int codigo;
+
+	private String response;
+
+	public RespuestaGenerica() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+}
